12da11e120ac5f46a771fcb2d181a91175039820
AdvantIoT <vikas.mujumdar@advantiot.com>
2022-11-03 14:23:28 +0530

    First commit.

---
+++ hello_world.js
@@ -0,0 +1,34 @@
+/* 
+ This is a very simple program that does nothing but print
+ Hello World to the console. It has been a traditional approach for
+ many years to start learning a new language with a Hello World program.
+ 
+ This will get you started on understanding and using the IDE
+ and making sure your development environment is working fine.
+  
+ First thing to learn, these lines of text starting with 
+ a forward slash (/) and an asterisk (*) 
+ and ending with an asterisk (*) and a forward slash (/)
+ are comments. These are used to explain the lines of code and
+ are ignored by the compiler and computer when executing the code.
+ You can type anything in comments, except the comment start 
+ and end markers.
+*/
+
+// Starting a line with two forward slashes is another way to write a comment
+// This is better for single line comments, while the previous approach 
+// is better for multi-line comments.
+
+// The actual program starts now:
+// START PROGRAM
+
+let text_to_print = "Hello World" // Define a variable and assign it a text value
+
+console.log(text_to_print); // Print the text to the console
+
+// END PROGRAM
+// To open the console type ctrl+` and go to the DEBUG CONSOLE tab
+
+// Run the program by pressing the F5 key (could be Fn+F5 or just F5 depending on your keyboard configuration)
+// Select the Node.js compiler if asked and see the display in the console.
+
